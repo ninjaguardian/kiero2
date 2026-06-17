279f048de6e59b35ccd25d2b9a1182fdfbd3d628
ninjaguardian <110734253+ninjaguardian@users.noreply.github.com>
2026-06-17 18:49:45 -0500

    Remove GetModuleHandleA

--- kiero_d3d11.cpp
+++ kiero_d3d11.cpp
@@ -4,49 +4,11 @@
 #include "kiero_d3d11.hpp"
 #include "kiero_intern.hpp"
 
-using CreateDXGIFactory_t =
-  HRESULT(WINAPI*)(REFIID riid, void** ppFactory);
-
-using D3D11CreateDeviceAndSwapChain_t = HRESULT(WINAPI*)(
-  IDXGIAdapter*,
-  D3D_DRIVER_TYPE,
-  HMODULE,
-  UINT,
-  const D3D_FEATURE_LEVEL*,
-  UINT,
-  UINT,
-  const DXGI_SWAP_CHAIN_DESC*,
-  IDXGISwapChain**,
-  ID3D11Device**,
-  D3D_FEATURE_LEVEL*,
-  ID3D11DeviceContext**
-);
 
 kiero::Error kiero::locate2(void* in, void* out)
 {
   KIERO_UNUSED(in);
   KIERO_UNUSED(out);
-
-  auto dxgi_dll = GetModuleHandleA("dxgi.dll");
-  if (!dxgi_dll) {
-    KIERO_DBG_MSG("dxgi.dll not loaded");
-    return Error_ModuleNotFound;
-  }
-
-  auto d3d11_dll = GetModuleHandleA("d3d11.dll");
-  if (!d3d11_dll) {
-    KIERO_DBG_MSG("d3d11.dll not loaded");
-    return Error_ModuleNotFound;
-  }
-
-  auto CreateDXGIFactory = (
-    (CreateDXGIFactory_t)
-    GetProcAddress(dxgi_dll, "CreateDXGIFactory")
-  );
-  if (!CreateDXGIFactory) {
-    KIERO_DBG_MSG("CreateDXGIFactory not found");
-    return Error_MethodNotFound;
-  }
 
   IDXGIFactory* factory;
   auto hresult = CreateDXGIFactory(
@@ -58,15 +20,6 @@ kiero::Error kiero::locate2(void* in, void* out)
     return Error_D3D11_CreateDXGIFactoryFailed;
   }
   KIERO_DEFER([&]() { factory->Release(); });
-
-  auto D3D11CreateDeviceAndSwapChain =(
-    (D3D11CreateDeviceAndSwapChain_t)
-    GetProcAddress(d3d11_dll, "D3D11CreateDeviceAndSwapChain")
-  );
-  if (!D3D11CreateDeviceAndSwapChain) {
-    KIERO_DBG_MSG("D3D11CreateDeviceAndSwapChain not found");
-    return Error_MethodNotFound;
-  }
 
   kiero::_::DummyWin32Window window;
   kiero::_::create_dummy_win32_window(&window);
